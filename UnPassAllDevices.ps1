41a61f74572b08f103668bf76f79d5457a1d1990
BSalita <BSalita@yahoo.com>
2021-02-08 16:48:20 +0100

    Create UnPassAllDevices.ps1

---
+++ UnPassAllDevices.ps1
@@ -0,0 +1,15 @@
+# Help script for unpassing all assigned devices in VM returning them to host.
+# Only processes devices which are in a disabled state.
+
+# configure these parameters
+$VM = 'Win-VM' # name of VM
+Write-Host "All devices assigned to $VM will be returned to host"
+
+# Remove all devices from a single VM
+Remove-VMAssignableDevice -VMName $VM -Verbose
+
+# Return all devices to host
+Get-VMHostAssignableDevice | Mount-VmHostAssignableDevice -Verbose
+
+# Enable devices currently disable ($_.Problem -eq 22).
+Get-PnpDevice | Where-Object {$_.Problem -eq 22} | Enable-PnpDevice -Confirm:$false
